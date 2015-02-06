f5e0b791d231035ffbe5b7db893e64eb383dc08e
t-yamamoto <t-yamamoto@itsol.co.jp>
2015-02-06 18:25:57 +0900

    intent用定数を外出し

---
+++ app/src/main/java/com/takawo/fan/FanConst.java
@@ -0,0 +1,11 @@
+package com.takawo.fan;
+
+/**
+ * Created by 9004027600 on 2015/02/06.
+ */
+public class FanConst {
+    static public String INTENT_PLAYER_ID = "playerId";
+    static public String INTENT_PLAYER_NAME = "playerName";
+    static public String INTENT_PLAYER_IMAGE = "playerImage";
+    static public String INTENT_RESULT_TYPE = "resultType";
+}
